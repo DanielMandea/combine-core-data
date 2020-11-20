ca9e3ee14167e65cb8ffb97da2a0f008b397af60
VladSima <>
2020-11-20 21:26:37 +0200

    Initial Commit For Test App

---
+++ CombineCoreDataTest/CombineCoreDataTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineCoreDataTest
+//
+//  Created by Sima Vlad Grigore on 20/11/2020.
+//  Copyright © 2020 Sima Vlad Grigore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
